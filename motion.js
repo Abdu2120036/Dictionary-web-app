a5d7fe188a669697952653fdf24e04d09342c32f
Abdulrahman Ben Omar <abdulrahmanbenomar@gmail.com>
2024-04-30 21:06:46 +0200

    Added definitions

--- motion.js
+++ motion.js
@@ -1,9 +1,24 @@
 const toggleBtn = document.querySelector(".toggle");
 const indicator = document.querySelector(".indicator");
 const body = document.querySelector("body");
-const playBtn = document.querySelector("#play-btn");
+
+const searchInput = document.getElementById('searchBar');
+
+const word = document.getElementById('word');
+const phonic = document.getElementById('phonic');
+const playBtn = document.getElementById("play-btn");
+
+const meaningNoun = document.querySelector('.sect-3-noun .meaning-text');
+const antonymsNoun = document.querySelector('.sect-3-noun .antonyms-text');
+const synonymsNoun = document.querySelector('.sect-3-noun .synonyms-text');
+
+const meaningVerb = document.querySelector('.sect-4-verb .meaning-text');
+const antonymsVerb = document.querySelector('.sect-4-verb .antonyms-text');
+const synonymsVerb = document.querySelector('.sect-4-verb .synonyms-text');
+
 const link = document.querySelector(".link");
 
+const url = 'https://api.dictionaryapi.dev/api/v2/entries/en/<word>'
 
 
 
