437a716e0a4ac5a06b6eeec63d82211a177a581e
Abdulrahman Ben Omar <abdulrahmanbenomar@gmail.com>
2024-04-30 09:33:57 +0200

    Finished the dark mode

--- motion.js
+++ motion.js
@@ -1,5 +1,17 @@
-const toggle = document.querySelector(".toggle");
+const toggleBtn = document.querySelector(".toggle");
+const indicator = document.querySelector(".indicator");
+const body = document.querySelector("body");
+const playBtn = document.querySelector("#play-btn");
+const link = document.querySelector(".link");
 
-toggle.addEventListener("click", () => {
-    toggle.classList.toggle('active')
+
+
+
+toggleBtn.addEventListener("click", () => {
+    toggleBtn.classList.toggle('active');
+    toggleBtn.classList.toggle('toggle-dark');
+    indicator.classList.toggle('indicator-dark');
+    playBtn.classList.toggle('dark');
+    link.classList.toggle('dark');
+    body.classList.toggle('dark-mode-all');
 });
